b9b2b3d798197346552a9456c9feb3d3dfb03b67
В. Александр <spbave@mail.ru>
2022-06-24 13:53:38 +0300

    добавлены локаторы для страницы логина, описана логика проверок страницы логина

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .main_page import MainPage
+from .locators import LoginPageLocators
+
+
+class LoginPage(MainPage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "/login is not in url"
+        assert True
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert True
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+        assert True
