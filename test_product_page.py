d770c5f9b0ef2d0816acfe0b1d8bf8512e783f28
В. Александр <spbave@mail.ru>
2022-06-28 21:11:03 +0300

    тест причесан

--- test_product_page.py
+++ test_product_page.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.by import By
 import pytest
 
 
-@pytest.mark.parametrize('link', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
+@pytest.mark.parametrize('link',
+                         ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, link):
     browser.delete_all_cookies()
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
@@ -12,12 +13,8 @@ def test_guest_can_add_product_to_basket(browser, link):
     product_page.should_be_add_to_basket()
     product_page.solve_quiz_and_get_code()
 
+    assert browser.find_element(By.XPATH, "//div[@id='messages']/div[1]"), "Сообщение о добавлении в корзину не найдено"
 
-# def test_guest_message_about_add_product_to_basket(browser):
-    assert browser.find_element(By.XPATH, "//div[@id='messages']/div[1]")
-
-
-# def test_guest_product_name_same_that_add(browser):
     product_name = browser.find_element(By.XPATH, "//div[@class='col-sm-6 product_main']/h1")
     product_name_text = product_name.text
     product_name_in_message = browser.find_element(By.XPATH,
@@ -25,12 +22,8 @@ def test_guest_can_add_product_to_basket(browser, link):
     product_name_in_message_text = product_name_in_message.text
     assert product_name_text == product_name_in_message_text, 'Название товара в сообщении не совпадает с тем что добавляли'
 
+    assert browser.find_element(By.XPATH, "//div[@id='messages']/div[3]"), "Сообщение о стоимости корзины не найдено"
 
-# def test_guest_message_about_basket_cost(browser):
-    assert browser.find_element(By.XPATH, "//div[@id='messages']/div[3]")
-
-
-# def test_guest_basket_cost_same_product_price(browser):
     product_price = browser.find_element(By.XPATH, "//div[@class='col-sm-6 product_main']/p[@class='price_color']")
     product_price_text = product_price.text
     product_price_in_basket = browser.find_element(By.XPATH,
